4f40ad3be885f71e71e00b46819bcf412b4551f3
Tim17033 <wachtim9@gmail.com>
2025-01-20 10:53:46 +0100

    Update bauspar.py

--- bauspar.py
+++ bauspar.py
@@ -71,8 +71,8 @@ def show_tarif_details(tarif_name, sparzins, regelsparbeitrag, abschlussgebuehr,
 def tarif_rechner(name, sparzins, regelsparbeitrag, abschlussgebuehr, jahresentgelt, zins_tilgung, darlehenszins):
     st.title(f"🏠 LBS Bausparrechner – {name}")
 
-    # Tarifdetails zuerst anzeigen (über allen Eingabefeldern)
-    st.markdown("### Eckdaten zum Tarif")
+    # Tarifdetails zuerst anzeigen (ganz oben über allen Eingabefeldern)
+    st.markdown("### Tarifkonditionen")
     bausparsumme = st.number_input("💰 Bausparsumme (€):", min_value=10000, max_value=500000, step=1000)
     einmalzahlung = st.number_input("💵 Einmalzahlung (€):", min_value=0.0, step=100.0)
     show_tarif_details(
@@ -167,6 +167,7 @@ elif tarif == "Classic20 Plus F":
     tarif_rechner("Classic20 Plus F", 0.01, 4, 1.6, 0.30, 5, 1.65)
 elif tarif == "Spar25":
     tarif_rechner("Spar25", 0.25, 5, 1.6, 0.30, 6, 4.25)
+
 
 
 
